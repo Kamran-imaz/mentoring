ed92516929f46ce3454f6106fa5838a647d69eac
Kamran-imaz <kamranimaz.professional@gmail.com>
2023-12-02 23:48:10 +0530

    home part of mentor completed

--- src/components/mentor/Home.jsx
+++ src/components/mentor/Home.jsx
@@ -4,53 +4,6 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const MentorHome = () => {
-<<<<<<< HEAD
-  const [display, setDisplay] = useState();
-  // const [rollno,setRollNo]=useState('');
-  // const handleClick=(roll)=>{
-  //   setRollNo(roll)
-  // }
-  // useEffect(() => {
-  //   const fetchData=async()=>{
-  //     const token=localStorage.getItem('auth-token');
-  //     if(token){
-  //       try{
-  //         const response=await axios.post('',{rollno},{
-  //           headers:{
-  //             'auth-token':token
-  //           }
-  //         })
-  //         const {success,message}=response.data;
-  //         if(success){
-
-  //         }
-  //         else{
-  //           console.log(message)
-  //         }
-  //       }
-  //       catch(err){
-  //         console.log(err)
-  //       }
-  //     }
-  //     else{
-  //       console.log("no token available")
-  //     }
-  //   }
-  //   fetchData();
-  // },[rollno]
-  // );
-  useEffect(() => {
-    const fetchData = async () => {
-      const token = localStorage.getItem("auth-token");
-      if (token) {
-        try {
-          const response = await axios.get(
-            "http://localhost:80/api/mentor/auth/getStudents",
-            {
-              headers: {
-                "auth-token": token,
-              },
-=======
     const [students, setStudents] = useState();
     const [rollno, setRollNo] = useState('');
     const handleClick = (roll) => {
@@ -79,7 +32,6 @@ const MentorHome = () => {
                 } catch (err) {
                     console.log(err);
                 }
->>>>>>> 16d9581041f47c643f7c96599cc948d8436dba4d
             }
         };
         fetchData();
@@ -156,49 +108,8 @@ const MentorHome = () => {
                     </table>
                 </div>
             </div>
-<<<<<<< HEAD
-          </div>
-        </div>
-        
-        <table className="min-w-full border border-black">
-          <thead>
-            <tr>
-              <th className="border p-2 border-black text-center">S.No</th>
-              <th className="border p-2 border-black text-center">RollNo</th>
-              <th className="border p-2 border-black text-center">Name</th>
-              <th className="border p-2 border-black text-center">
-                Approvals Left
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Adjust mapping here */}
-            {display ? (
-              display.map((ele, index) => (
-                <tr key={index}>
-                  <td className="border p-2 border-black text-center">{index + 1}</td>
-                  <td className="border p-2 border-black text-center"><button className="hover:bg-blue-800 hover:text-white transition duration-300 px-3 py-2 rounded" ><Link to={`/combineLinks/${ele.rollNo}`}>{ele.rollNo}</Link></button></td>
-                  {/* {console.log(ele.rollNo)} */}
-                  <td className="border p-2 border-black text-center">{ele.name}</td>
-                  <td className="border p-2 border-black text-center">1</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="4" className="text-center">
-                  Not Found
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
-=======
         </>
     );
->>>>>>> 16d9581041f47c643f7c96599cc948d8436dba4d
 };
 
 export default MentorHome;
